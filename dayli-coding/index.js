a87ca97c441655dd3cfd3bbdfc29679117b3c003
THISmann <fuhetienne0@gmail.com>
2022-04-19 16:52:13 +0100

    init

---
+++ dayli-coding/index.js
@@ -0,0 +1,36 @@
+// exercise 1 
+
+let fruits = ["bannana", "apples" , "oranges" , "BLeuberries"];
+
+// question 1
+fruits.pop();
+
+// question 2 
+fruits.sort();
+
+// question 3
+fruits.push("wiki");
+
+// question 4 
+fruits.splice(0,1);
+
+//question 5
+fruits.reverse();
+
+
+
+// exercise 2 
+let moreFruits = ["Banana" , ["Apples", ["Oranges"], "Bleuberries"]];
+console.log(moreFruits[1][1])
+
+ 
+
+// Dayli challenge 2
+// question 1 
+
+let array = ["*", "*" , "*", "*" , "*"];
+
+for (let index = 0; index < array.length; index++) {
+    console.log("*");
+}
+
